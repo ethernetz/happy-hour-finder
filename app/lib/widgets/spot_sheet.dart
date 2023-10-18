ac5881f4cc5bf6c190c7b3c5d70c824f499dacd8
Ethan Netz <ethannetz@hotmail.com>
2023-10-17 23:44:29 -0700

    try with verbose: true

---
+++ app/lib/widgets/spot_sheet.dart
@@ -0,0 +1,64 @@
+import 'package:app/providers/map_visible_region_places_provider.dart';
+import 'package:app/spot_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SpotSheet extends StatelessWidget {
+  const SpotSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MapVisibleRegionPlacesProvider>(
+        builder: (context, provider, child) {
+      final spots = provider.allSpots;
+      return DraggableScrollableSheet(
+        initialChildSize: 0.5,
+        minChildSize: 0.1,
+        maxChildSize: 1.0,
+        snap: true,
+        snapSizes: const [0.1, 0.5, 1.0],
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16.0),
+                topRight: Radius.circular(16.0),
+              ),
+            ),
+            child: ListView(
+              controller: scrollController,
+              padding: const EdgeInsets.only(top: 10.0),
+              children: [
+                // Handle
+                Center(
+                  child: Container(
+                    width: 40,
+                    height: 4,
+                    margin: const EdgeInsets.symmetric(vertical: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(2.0),
+                    ),
+                  ),
+                ),
+                // List items
+                ...spots.map((spot) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      child: SpotCard(
+                        spot: spot,
+                      ),
+                    )),
+              ],
+            ),
+          );
+        },
+      );
+    });
+  }
+}
