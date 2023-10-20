858dbc1eaf4de9f1385860094fa4932945150a40
Ethan Netz <ethannetz@hotmail.com>
2023-10-19 17:21:10 -0700

    add flutter_map_marker_cluster

--- app/lib/widgets/map.dart
+++ app/lib/widgets/map.dart
@@ -7,6 +7,7 @@ import 'package:app/providers/map_visible_region_places_provider.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -54,18 +55,57 @@ class MapState extends State<Map> {
       final spots = provider.allSpots;
       return FlutterMap(
         mapController: MapController(),
-        options: const MapOptions(
-          initialCenter: LatLng(40.776676, -73.971321),
-          initialZoom: 8,
+        options: MapOptions(
+          initialCenter: const LatLng(40.776676, -73.971321),
+          initialZoom: 14,
+          onPositionChanged: (position, hasGesture) {
+            provider.updateLocation(position.bounds!);
+          },
+          cameraConstraint: CameraConstraint.containCenter(
+              bounds: LatLngBounds(
+            const LatLng(40.821669, -74.016571),
+            const LatLng(40.697885, -73.909383),
+          )),
         ),
         children: [
           TileLayer(
             urlTemplate:
                 'https://api.mapbox.com/styles/v1/ethernetz/clnxlcfzp002b01r73s4h3g2b/tiles/256/{z}/{x}/{y}@2x?access_token=${Env.mapboxAPIKey}',
             additionalOptions: {
-              'accessToken': '${Env.mapboxAPIKey}',
+              'accessToken': Env.mapboxAPIKey,
               'id': 'mapbox.mapbox-streets-v8',
             },
+          ),
+          MarkerClusterLayerWidget(
+            options: MarkerClusterLayerOptions(
+              maxClusterRadius: 45,
+              size: const Size(40, 40),
+              alignment: Alignment.center,
+              markers: spots.values.map((spot) {
+                return Marker(
+                  width: 40,
+                  height: 40,
+                  point: spot.coordinates,
+                  child: IconButton(
+                    icon: const Icon(Icons.location_on),
+                    onPressed: () {},
+                  ),
+                );
+              }).toList(),
+              builder: (context, markers) {
+                return Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue),
+                  child: Center(
+                    child: Text(
+                      markers.length.toString(),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
           CurrentLocationLayer(),
         ],
